259dd6d37a714fc85cd6653d9027b2bd084582b1
tanvir <tanvirgeek@gmail.com>
2020-11-02 07:27:24 +0600

    Initial Commit

---
+++ FoodOrderingApp/FoodOrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodOrderingAppApp.swift
+//  FoodOrderingApp
+//
+//  Created by MD Tanvir Alam on 2/11/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodOrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
