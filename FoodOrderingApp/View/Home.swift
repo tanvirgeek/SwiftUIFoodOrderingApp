2833473d0ce755e16f1eb442096a77780bdb6cec
tanvir <tanvirgeek@gmail.com>
2020-11-04 08:03:38 +0600

    Food Ordering App Day2

--- FoodOrderingApp/View/Home.swift
+++ FoodOrderingApp/View/Home.swift
@@ -10,49 +10,60 @@ import SwiftUI
 struct Home: View {
     @StateObject var homeVM =  HomeViewModel()
     var body: some View {
-        VStack(){
-            HStack(spacing: 15){
-                Button {
-                    print("Button Clicked")
-                } label: {
-                    Image(systemName: "line.horizontal.3")
-                        .font(.title)
-                        .foregroundColor(.pink)
+        
+        ZStack{
+            
+            VStack(){
+                HStack(spacing: 15){
+                    Button {
+                        print("Button Clicked")
+                    } label: {
+                        Image(systemName: "line.horizontal.3")
+                            .font(.title)
+                            .foregroundColor(.pink)
+                    }
+                    Text( homeVM.userLocation == nil ? "Locating...":"Deliver to")
+                        .foregroundColor(.black)
+                    
+                    Text(homeVM.userAddress)
+                        .font(.caption)
+                        .fontWeight(.heavy)
+                    
+                    Spacer()
                 }
-                Text("Deliver to")
-                    .foregroundColor(.black)
+                .padding([.horizontal,.top])
                 
-                Text("Apple")
-                    .font(.caption)
-                    .fontWeight(.heavy)
+                Divider()
                 
+                HStack(){
+                    TextField("Search", text:$homeVM.search)
+                    if(homeVM.search != ""){
+                        Button(action:{}, label:{
+                            
+                            Image(systemName: "magnifyingglass")
+                                .font(.title)
+                                .foregroundColor(.gray)
+                            
+                        }).animation(.easeIn)
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top,10)
+                
+                Divider()
                 Spacer()
             }
-            .padding([.horizontal,.top])
-            
-            Divider()
-            
-            HStack(){
-                TextField("Search", text:$homeVM.search)
-                if(homeVM.search != ""){
-                    Button(action:{}, label:{
-                        
-                        Image(systemName: "magnifyingglass")
-                            .font(.title)
-                            .foregroundColor(.gray)
-                        
-                    }).animation(.easeIn)
-                }
+            .onAppear {
+                homeVM.locationManager.delegate = homeVM
+                
             }
-            .padding(.horizontal)
-            .padding(.top,10)
             
-            Divider()
-            Spacer()
-        }.onAppear {
-            homeVM.locationManager.delegate = homeVM
-            homeVM.locationManager.requestWhenInUseAuthorization()
+            if homeVM.noLocation{
+                Text("Please enable location access to settings to furthur move on")
+                    .foregroundColor(.black)
+            }
         }
+        
     }
 }
 
--- FoodOrderingApp/ViewModel/HomeViewModel.swift
+++ FoodOrderingApp/ViewModel/HomeViewModel.swift
@@ -12,20 +12,46 @@ class HomeViewModel:NSObject, ObservableObject, CLLocationManagerDelegate{
     
     @Published var search = ""
     @Published var locationManager = CLLocationManager()
+    @Published var userLocation:CLLocation!
+    @Published var userAddress = ""
+    @Published var noLocation = false
     
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         switch manager.authorizationStatus {
         case .authorizedWhenInUse:
             print("Authorized")
+            self.noLocation = false
+            manager.requestLocation()
         case .denied:
             print("Denied")
+            self.noLocation = true
         default:
             print("Unknown")
+            self.noLocation = false
+           locationManager.requestWhenInUseAuthorization()
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error.localizedDescription)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.userLocation = locations.last
+        self.extractLocation()
+    }
+    func extractLocation(){
+        CLGeocoder().reverseGeocodeLocation(self.userLocation){ res , error in
+            guard let safedata = res else{return}
+            var address = ""
+            
+            address += safedata.first?.name ?? ""
+            address += ", "
+            address += safedata.first?.locality ?? ""
+            
+            self.userAddress = address
+            
+        }
     }
     
 }
